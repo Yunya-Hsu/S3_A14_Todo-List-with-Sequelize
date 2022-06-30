58144e736be1961fe999055a1f260718cbcfaf4e
Yunya Hsu <yunyahsu110@gmail.com>
2022-06-30 16:14:29 +0800

    feat: add login & register router

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ const session = require('express-session')
 const passport = require('passport')
 
 const PORT = 3000
+const routers = require('./routers/index')
 
 const app = express()
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main', }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(routers)
 
 app.listen(PORT, () => {
   console.log(`Express is running on localhost: ${PORT}`)
