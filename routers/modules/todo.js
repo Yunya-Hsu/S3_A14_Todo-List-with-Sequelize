b4a36e4dc123ee11dee2be65338347512e797eb5
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-02 12:52:38 +0800

    feat: add create page & create feature

--- routers/modules/todo.js
+++ routers/modules/todo.js
@@ -5,6 +5,23 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name // 從req.body拿出表單裡的name資料
+  const userId = req.user.id
+  return Todo.create({
+    name,
+    UserId: userId,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
