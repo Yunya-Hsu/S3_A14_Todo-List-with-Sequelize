cdd75d8caf0eb50d6b678520da2507aed59b0edc
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-02 11:25:35 +0800

    feat: add passport to login

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 const session = require('express-session')
-const passport = require('passport')
 
 const PORT = 3000
 const routers = require('./routers/index')
+const usePassport = require('./config/passport')
 
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(session({
+  secret: 'asdfghjk',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 app.use(routers)
 
--- routers/index.js
+++ routers/index.js
@@ -5,8 +5,10 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const todo = require('./modules/todo')
 
-router.use('/todos', todo)
+const { authenticator } = require('../middleware/auth')
+
+router.use('/todos', authenticator, todo)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
